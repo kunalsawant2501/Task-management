db28c4a079fb25f5e18c39b9cb30f7895d42110c
vijendra <110163856+yatinbisht76pcg@users.noreply.github.com>
2025-06-04 17:07:36 +0530

    fetching user according to roles to assign task by admin or manager

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,7 +2,14 @@ const User = require("../models/User");
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({});
+    const role = req.user.role
+
+    let users;
+    if(role === "admin"){
+      users = await User.find({})
+    }else{
+      users = await User.find({ role: "user"})
+    }
     res.status(200).json({ message: "workong fine", users: users });
   } catch (error) {
     res.status(500).json({ message: "serverError" });
